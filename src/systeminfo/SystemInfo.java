ec93bb0cee158212076e63317face867f601e05b
WilloIzCitron(AetherLumine) <49786146+WilloIzCitron@users.noreply.github.com>
2024-05-03 21:51:44 +0700

    yes

--- src/systeminfo/SystemInfo.java
+++ src/systeminfo/SystemInfo.java
@@ -17,18 +17,21 @@ public class SystemInfo extends Mod{
         Log.info("Loaded ExampleJavaMod constructor.");
     }
     @Override
-    public void init(){
+    public void init() {
+
         systemInfo = new BaseDialog("System Info");
         systemInfo.cont.image(Core.atlas.find(osIndent())).maxHeight(100).maxWidth(500).row();
-        systemInfo.cont.add("OS: "+ OS.osName+ " ( "+OS.osArch+" | "+OS.osArchBits+"-bit )").row();
-        systemInfo.cont.add("Java: "+System.getProperty("java.vendor")+" "+System.getProperty("java.version")).row();
-        systemInfo.cont.add("Java VM: "+System.getProperty("java.vm.vendor")+" "+System.getProperty("java.vm.version")).row();
+        if (OS.isAndroid) {
+            systemInfo.cont.add("OS: Android ( " + OS.osArch + ")").row();
+        } else{
+            systemInfo.cont.add("OS: " + OS.osName + " ( " + OS.osArch + " | " + OS.osArchBits + "-bit )").row();
+        }
+        systemInfo.cont.add("Java VM: "+System.getProperty("java.vm.vendor")+" ("+System.getProperty("java.vm.name")+") "+System.getProperty("java.vm.version")).row();
         systemInfo.addCloseButton();
         loadButton();
     }
     void loadButton(){
         ui.settings.button(Icon.info, systemInfo::show).row();
-        ;
     }
     @Override
     public void loadContent(){
@@ -44,7 +47,7 @@ public class SystemInfo extends Mod{
             }
         } else if(OS.isAndroid){
             //vendor based
-            if(System.getProperty("http.agent").equals("Infinix")){
+            if(System.getProperty("http.agent").contains("Infinix")){
                 return "systeminfo-infinix";
             }
             return "systeminfo-android";
